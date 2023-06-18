6838b6efad5e73dd95ff7b356262aadd8d26c6bf
Amol68 <109221958+Amol68@users.noreply.github.com>
2023-06-18 23:02:02 +0530

    program to push element at stack bottom
    program to push element at stack bottom

--- Stack/solution2.java
+++ Stack/solution2.java
@@ -39,3 +39,36 @@ class solution2{
     }
     
 }
+
+_ _ _ _ _ _ _ _ \\ _ _ _ _ _ _ _ _ \\ _ _ _ _ _ _ _ _ \\ _ _ _ _ _ _ _ _ \\ _ _ _ _ _ _ _ _ \\ _ _ _ _ _ _ _ _ \\ _ _ _ _ _ _ _ _ \\
+    USING RECUSRSION
+
+import java.util.Stack;
+class Solution2 {
+    
+    static void push_at_bottom(Stack<Integer> S , int n){
+        
+        if(S.isEmpty()){
+            S.push(n);
+            return;
+        }
+        
+        int top = S.pop();
+        push_at_bottom(S , n);
+        S.push(top);
+    }
+    
+    public static void main(String[] args) 
+    {
+        Stack<Integer> s1 = new Stack<>();
+        s1.push(20);
+        s1.push(30);
+        s1.push(40);
+        
+        push_at_bottom(s1 , 10);
+        while(!s1.isEmpty()){
+           System.out.println(s1.peek());   
+        }
+        
+    }
+}
