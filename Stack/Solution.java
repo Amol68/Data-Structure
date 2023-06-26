52403bae1aab916e55729e6b5b98f33a24e9e946
Amol68 <109221958+Amol68@users.noreply.github.com>
2023-06-26 22:56:39 +0530

    next greater element to right
    next greater element to right

--- Stack/Solution.java
+++ Stack/Solution.java
@@ -1,31 +1,36 @@
-class Solution {
-    public int[] nextGreaterElement(int[] arr) {
-        
-     int[] result = new int[arr.length];
-     Stack<Integer> stack = new Stack<>();
+import java.util.*;
 
-    for(int i=arr.length-1; i>=0; i--){
-
-      if(!stack.isEmpty()){
-
-        while(!stack.isEmpty() && arr[i]>=stack.peek()){
-            stack.pop();
-        }
-
-        if(stack.isEmpty()){
-            result[i]=-1;
-        }
-        else{
-            result[i]=stack.peek();
-        }
-
-   
-      }
-
-    stack.push(arr[i]);
-
-    }
-
-    return result;
+class HelloWorld {
+    public static void main(String[] args) {
+       
+       int arr[] = {6,4,9,12,11,55};
+       int next_greater[] = new int[arr.length];
+       Stack<Integer> s1 = new Stack<>();
+       
+       
+       for(int i = arr.length-1; i>=0; i--){
+           
+             // push index of element in stack rather than element itself
+           
+             while(!s1.isEmpty() && arr[s1.peek()]<=arr[i]){  
+                s1.pop();
+             }
+           
+           if(s1.isEmpty()){  // no greater element to right
+               next_greater[i]= -1;
+           }
+               
+           else{
+                next_greater[i]=arr[s1.peek()];
+               }
+           
+           s1.push(i); // push index of element in stack rather than element itself
+       }
+       
+       for(int i=0; i<next_greater.length; i++){
+           
+           System.out.println(next_greater[i]+" ");
+           
+       }
     }
 }
