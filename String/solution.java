7ed235d741b605890f2c3ce9f9b924405476f07e
Amol68 <109221958+Amol68@users.noreply.github.com>
2023-07-04 22:11:03 +0530

    ParseInt and ToString Method
    ParseInt and ToString Method

---
+++ String/solution.java
@@ -0,0 +1,15 @@
+
+
+
+class solution {
+    public static void main(String[] args) {
+       
+       String str = "123";
+       int num = Integer.parseInt(str);
+       System.out.println(num);
+       
+       int num1 = 123;
+       String str1 = Integer.toString(num1);
+        System.out.println(str1.length());
+    }
+}
