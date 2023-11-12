d0ce011c3b31f07e310cc9d0b6ceb43b81f8aaff
Amol68 <109221958+Amol68@users.noreply.github.com>
2023-11-12 15:24:37 +0530

    group anagrams solution
    group anagrams solution

--- HashMap/group_anagrams.java
+++ HashMap/group_anagrams.java
@@ -1,13 +1,49 @@
+
+  // Easier Approach
+
 import java.util.*;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
     
-      // Steps
+        HashMap<String , List<String>> mp = new HashMap<>();
+
+
+        // Steps
       // -> Loop through the given string array
       // -> sort the strings
       // -> insert sorted string in hmap as key along with corresponding string as values
       // -> Iterate through hmap
+        
+              for(String s : strs){
+                  char[] arr = s.toCharArray();
+                  Arrays.sort(arr);
+                  String sorted_string = new String(arr);
+
+                   if(!mp.containsKey(sorted_string)){
+                       mp.put(sorted_string,new ArrayList<>());
+                   }
+
+                     mp.get(sorted_string).add(s);
+              }
+
+              return new ArrayList<>(mp.values());
+    
+        }
+        
+    }
+
+
+
+---------->---------->---------->---------->---------->---------->---------->---------->---------->---------->---------->---------->---------->---------->---------->---------->---------->
+
+  
+
+
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
 
       HashMap<String,List<String>> map = new HashMap<>();
       List<List<String>> final_list = new ArrayList<>();
@@ -34,3 +70,4 @@ class Solution {
         }
         
     }
+
