8b96895a264b3a0bfe54c7257273c38e10fa7806
Amol68 <109221958+Amol68@users.noreply.github.com>
2024-02-01 21:36:11 +0530

    skip a character from string and return a new string
    skip a character from string and return a new string

---
+++ Recursion/Solution_15.java
@@ -0,0 +1,81 @@
+class HelloWorld {
+
+ 
+    public static void main(String[] args) {
+         
+         StringBuilder sb = new StringBuilder();
+         String str ="baccad";
+         char[] arr = str.toCharArray();
+         
+         for(int i=0; i<arr.length; i++){
+             if(arr[i]!='a'){
+                 sb.append(arr[i]);
+             }
+         }
+         
+          System.out.println(sb);
+         
+    }
+}
+
+
+//-------->-------->-------->-------->-------->USING RECURSION-------->-------->-------->-------->-------->-------->-------->
+
+
+import java.util.*;
+
+class HelloWorld {
+ 
+    public static void main(String[] args) {
+         
+        String str = "baccdh";
+        remove("",str);
+    }
+    
+    private static void remove(String p,String up){
+        
+        if(up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        
+        char ch = up.charAt(0);
+        if(ch=='a'){
+            remove(p,up.substring(1));
+        }
+        else{
+            remove(p+ch,up.substring(1));
+        }
+    }
+}
+
+
+
+//-------->-------->-------->-------->-------->USING RECURSION Another approach-------->-------->-------->-------->-------->-------->-------->
+
+
+class HelloWorld {
+
+  public static void main(String[] args) {
+
+    String str = "baccdh";
+    String result = remove(str);
+    System.out.println(result);
+  }
+
+  private static String remove(String up) {
+
+    if (up.isEmpty()) {
+      return "";
+    }
+
+    char ch = up.charAt(0);
+
+    if (ch == 'a') {
+      return remove(up.substring(1));
+    } else {
+      return remove(ch + up.substring(1));
+    }
+
+  }
+}
