83c5498b9a2156face9ee92e8f33a26555879abe
Amol68 <109221958+Amol68@users.noreply.github.com>
2024-02-05 21:02:28 +0530

    star pattern using recursion

--- Recursion/solution_10.java
+++ Recursion/solution_10.java
@@ -24,11 +24,43 @@ public class pattern {
        print(4,0);
 	}
 
-}
-
 // Output
 // *
 // **
 // ***
 // ****
+
+// --------->--------->--------->--------->--------->Inverse Triangle --------->--------->--------->--------->--------->--------->
+
+
+
+	class HelloWorld {
+    public static void main(String[] args) {
+       int limit = 5;
+       int arr
+       print(limit,0);
+       
+    }
+    
+   public static void print(int row,int col){
+       if(row==0) return;
+       if(col<row){
+           System.out.print("*");
+           print(row,col+1);
+       }
+       else{
+        System.out.println();
+          print(row-1,0);
+       }
+       
+   }
+
+}
+
+// Output
+// *****
+// ****
+// ***
+// **
+// *
 
