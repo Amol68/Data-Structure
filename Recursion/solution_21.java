9be48a832d4ca0375edb19779995d10ddce0610e
Amol68 <109221958+Amol68@users.noreply.github.com>
2024-04-08 21:13:25 +0530

    Dice throw
    Dice throw

---
+++ Recursion/solution_21.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+      System.out.println(dice("",4));
+    }
+    
+    public static ArrayList dice(String p,int target){
+        
+        if(target==0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        
+         ArrayList<String> list = new ArrayList<>();
+          
+        for(int i=1; i<=6 && i<=target; i++){
+           list.addAll(dice(p+i,target-i));
+           
+        }
+        return list;
+    }
+}
