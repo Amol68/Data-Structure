24b46448f27e7cd278449b4e729d751fa6532e58
Amol68 <109221958+Amol68@users.noreply.github.com>
2024-04-11 21:24:24 +0530

    Maze Problems

--- Recursion/solution_22.java
+++ Recursion/solution_22.java
@@ -36,6 +36,43 @@ class HelloWorld {
 
 
 import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+       
+     System.out.println(path("",3,3)) ;
+       
+    }
+    
+    public static ArrayList<String> path(String p,int r,int c){
+          
+       if(r==1 && c==1){
+           
+          ArrayList<String> list = new ArrayList<>();
+          list.add(p);
+          return list;
+       }
+       
+        ArrayList<String> list = new ArrayList<>();
+        
+        if(r>1){
+            list.addAll(path(p+'D',r-1,c));
+        }
+        
+         if(c>1){
+           list.addAll(path(p+'R',r,c-1));
+        }
+        
+        return list;
+        
+    }
+}
+
+
+// -------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>
+// here we are also considering diagonal
+
+
+import java.util.*;
 
 class HelloWorld {
     public static void main(String[] args) {
@@ -56,6 +93,10 @@ class HelloWorld {
        }
        
         ArrayList<String> list = new ArrayList<>();
+        
+        if(r>1 && c>1){
+            list.addAll(path(p+'C',r-1,c-1));
+        }
         
         if(r>1){
             list.addAll(path(p+'D',r-1,c));
