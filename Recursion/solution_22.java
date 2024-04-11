4a8469b3bcfbd9eee9912f5724bd0b675e123d0a
Amol68 <109221958+Amol68@users.noreply.github.com>
2024-04-11 20:54:16 +0530

    Maze problems (finding paths)

--- Recursion/solution_22.java
+++ Recursion/solution_22.java
@@ -30,3 +30,42 @@ class HelloWorld {
         
     }
 }
+
+
+// -------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>
+
+
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+       
+     System.out.println(path("",3,3)) ;
+       
+    }
+    
+    public static ArrayList<String> path(String p,int r,int c){
+        
+        
+        
+       if(r==1 && c==1){
+           
+          ArrayList<String> list = new ArrayList<>();
+          list.add(p);
+          return list;
+       }
+       
+        ArrayList<String> list = new ArrayList<>();
+        
+        if(r>1){
+            list.addAll(path(p+'D',r-1,c));
+        }
+        
+         if(c>1){
+           list.addAll(path(p+'R',r,c-1));
+        }
+        
+        return list;
+        
+    }
+}
