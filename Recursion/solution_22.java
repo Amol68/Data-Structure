b052613356b0a517b39c4f508cb595349e49682a
Amol68 <109221958+Amol68@users.noreply.github.com>
2024-04-13 20:54:40 +0530

    maze problems

--- Recursion/solution_22.java
+++ Recursion/solution_22.java
@@ -110,3 +110,50 @@ class HelloWorld {
         
     }
 }
+
+
+
+// -------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>
+// here we have a river in a cell
+
+
+
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+       
+       boolean[][] board ={
+           {true,true,true},
+           {true,false,true},
+           {true,true,true}
+       } ;
+       
+      path("",board,0,0);
+       
+    }
+    
+    public static void path(String p,boolean[][] maze,int r,int c){
+        
+        if(r==maze.length-1 && c==maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+        
+        if(!maze[r][c]){
+            return;
+        }
+        
+       
+        
+        if(r<maze.length-1){
+           path(p+"D",maze,r+1,c);
+        }
+        
+         if(c<maze[0].length-1){
+            path(p+"R",maze,r,c+1);
+        }
+        
+       
+    }
+}
