5df2ed1b4e038274ec34316086a81ec60388463d
Amol68 <109221958+Amol68@users.noreply.github.com>
2024-04-23 21:03:55 +0530

    maze problems

--- Recursion/solution_22.java
+++ Recursion/solution_22.java
@@ -157,3 +157,61 @@ class HelloWorld {
        
     }
 }
+
+
+
+// -------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>-------->>
+// While visiting a cell we mark it false and while going back from that cell we remark it as true.
+
+
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+       
+       boolean[][] board ={
+           {true,true,true},
+           {true,true,true},
+           {true,true,true}
+       } ;
+       
+      path("",board,0,0);
+       
+    }
+    
+    public static void path(String p,boolean[][] maze,int r,int c){
+        
+        if(r==maze.length-1 && c==maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+        
+      
+        
+        if(!maze[r][c]){
+            return;
+        }
+        
+         maze[r][c]=false;
+        
+        if(r<maze.length-1){
+           path(p+"D",maze,r+1,c);
+        }
+        
+         if(c<maze[0].length-1){
+            path(p+"R",maze,r,c+1);
+        }
+        
+         if(r>0){
+           path(p+"U",maze,r-1,c);
+        }
+        
+         if(c>0){
+            path(p+"L",maze,r,c-1);
+        }
+        
+         maze[r][c]=true;
+       
+    }
+   
+}
