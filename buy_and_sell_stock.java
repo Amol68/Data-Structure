9c9be4aba68ae79f6b1436af657341a8ccab0eef
Amol68 <109221958+Amol68@users.noreply.github.com>
2024-08-31 14:02:34 +0530

    naive and optimized solution

--- buy_and_sell_stock.java
+++ buy_and_sell_stock.java
@@ -1,3 +1,45 @@
+// naive approach
+
+class Solution {
+    public int maxProfit(int[] prices) {
+
+     int min = Integer.MAX_VALUE;
+     int min_index=-1;  // index of mininum element
+     int max_profit = 0;
+     int curr_profit;
+
+     for(int i=0; i<prices.length; i++){
+
+          if(prices[i]<min){
+             min=prices[i];
+             min_index=i;
+          } 
+ 
+     }   // for loop to find min element and its index;
+
+ 
+    if(min_index==prices.length-1) return 0;
+
+      for(int i=min_index; i<prices.length; i++){
+
+            if(prices[i]>min){
+
+                curr_profit=prices[i]-min;
+                 if(curr_profit>max_profit){
+                    max_profit=curr_profit;
+                 }
+            } 
+            
+      }
+       return max_profit;  
+    }
+}
+
+
+
+
+//----------||----------||----------||----------||----------||----------||----------||----------||----------||----------||----------||----------||----------||----------||----------||
+
 class Solution {
     public int maxProfit(int[] prices) {
 
