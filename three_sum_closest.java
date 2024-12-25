136d02079d4534802349114226ab6ee31567c9ea
Amol68 <109221958+Amol68@users.noreply.github.com>
2024-12-26 00:41:15 +0530

    three sum closest leetcode solution

--- three_sum_closest.java
+++ three_sum_closest.java
@@ -39,3 +39,53 @@ class Solution {
 
 
 //----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->----->
+
+// Optimozed Approach
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        
+         if(nums==null || nums.length<3) 
+          throw new IllegalArgumentException("Input array must have at least 3 elements.");
+
+        Arrays.sort(nums);
+        int length = nums.length;
+        int min_diff = Integer.MAX_VALUE;
+        int sum=nums[0]+nums[1]+nums[2];
+        
+
+        for(int i=0; i<length-2; i++){
+
+             if( (i>0) && nums[i]==nums[i-1]) continue;
+
+            int left = i+1;
+            int right = length-1;
+
+            while(left < right){
+
+                //if((left>0 && right<length-1) && (nums[left]!=nums[right])) continue;
+                int curr_sum = nums[i]+nums[left]+nums[right];
+
+                
+                if(curr_sum <target) left++;
+                if(curr_sum >target) right--; 
+
+                int diff = Math.abs(target-curr_sum );
+
+                if(diff==0) return curr_sum;
+
+                if(diff<min_diff){
+                    min_diff = diff;
+                    sum = curr_sum;    
+                }
+
+            }
+        }
+
+      return sum;
+                   
+    }
+}
+
+// Time Complexity: O(n^2)
+// Space Complexiyu: O(1)
