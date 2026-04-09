3055371338ee00a5067081bda3fba76551b12242
Amol68 <109221958+Amol68@users.noreply.github.com>
2026-04-10 00:28:03 +0530

    trapping rainwater

--- Stack/solution_14.js
+++ Stack/solution_14.js
@@ -22,3 +22,79 @@ for(let j=curr; j<heights.length; j++){
 
 // Time Complexity: O(n^2);
 // Space Complexity : O(n)
+
+// ----------------------> ----------------------> ----------------------> ----------------------> ---------------------->
+
+// Better Approach
+function trap(height: number[]): number {
+
+    if(!height || !height.length) return 0;
+
+    const leftMax:number[] = [];
+    const rightMax:number[] = [];
+    const n:number = height.length;
+    leftMax[0] = height[0];
+    rightMax[n - 1] = height[n - 1];
+    let totalWater:number = 0;
+
+    for (let i = 1; i < n ; i++) {
+        leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+    }
+
+    for (let i = n - 2; i >= 0; i--) {
+        rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+    }
+
+    for (let i = 0; i < n; i++) {
+        totalWater += Math.min(leftMax[i], rightMax[i]) - height[i];
+    }
+
+    return totalWater;
+
+};
+
+
+// Time Complexity: O(n);
+// Space Complexity : O(n)
+
+// ----------------------> ----------------------> ----------------------> ----------------------> ---------------------->
+
+// Optimal Approach
+function trap(height: number[]): number {
+
+    if (!height || !height.length) return 0;
+    const n:number = height.length;
+    let left:number = 0;
+    let right:number = n - 1;
+    let leftMax:number = height[0];
+    let rightMax:number= height[n - 1];
+    let result:number = 0;
+
+    while (left < right) {
+
+        if (leftMax < rightMax) {
+
+            left += 1;
+
+            leftMax = Math.max(leftMax, height[left]);
+
+            result += leftMax - height[left];
+
+        }
+        else {
+
+            right-=1;
+             rightMax=Math.max(rightMax,height[right]);
+             result+=rightMax-height[right]
+        }
+
+    }
+
+    return result;
+
+};
+
+
+// Time Complexity: O(n);
+// Space Complexity : O(1)
+
